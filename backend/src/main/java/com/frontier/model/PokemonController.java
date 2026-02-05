b7f4f36fc811a078b5d48e35cac9e6998e99f5de
Alex <aaahandour@gmail.com>
2026-02-05 17:37:52 -0500

    add test controller functions

--- backend/src/main/java/com/frontier/model/PokemonController.java
+++ backend/src/main/java/com/frontier/model/PokemonController.java
@@ -4,22 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 
 @Controller
+@AllArgsConstructor
 public class PokemonController {
 
-    private final PokemonRepository PokemonRepository;
+    private final PokemonRepository pokemonRepository;
 
-    public PokemonController(PokemonRepository pokemonRepository) {
-        this.PokemonRepository = pokemonRepository;
-    }
-
-    @GetMapping("/pokemon")
+    @GetMapping("/pokemonTest")
     public String getMethodName(@RequestParam String param) {
         return new String("This is a test of the GET /pokemon endpoint");
     }
+
+    @GetMapping("/pokemon/${id}")
+    public String getPokemon(@RequestParam String param) {
+        return pokemonRepository.getPokemonById();
+    }
+    
     
 }
--- backend/src/main/java/com/frontier/model/TrainerController.java
+++ backend/src/main/java/com/frontier/model/TrainerController.java
@@ -1,8 +1,20 @@
 package com.frontier.model;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class TrainerController {
+
+    private final TrainerRepository trainerRepository;
+
+    @GetMapping("/trainerTest")
+    public String getMethodName(@RequestParam String param) {
+        return new String("This is a test of the GET /trainer endpoint");
+    }
     
 }
