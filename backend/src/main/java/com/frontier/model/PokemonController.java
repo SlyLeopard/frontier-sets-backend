17a8487203be7f36a7808d3f851e0d7dd30005ad
Alex <aaahandour@gmail.com>
2026-02-27 17:04:47 -0500

    update controllers to be rest controllers instead of regular controllers

--- backend/src/main/java/com/frontier/model/PokemonController.java
+++ backend/src/main/java/com/frontier/model/PokemonController.java
@@ -1,13 +1,13 @@
 package com.frontier.model;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class PokemonController {
 
