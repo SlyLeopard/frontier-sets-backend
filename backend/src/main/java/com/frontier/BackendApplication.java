64bc164e33a620808eb29ee3519ccb75370a137f
Alex <aaahandour@gmail.com>
2026-02-28 01:40:31 -0500

    clean up code and add mongo repository for trainers

--- backend/src/main/java/com/frontier/BackendApplication.java
+++ backend/src/main/java/com/frontier/BackendApplication.java
@@ -2,8 +2,10 @@ package com.frontier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.frontier")
+@EnableMongoRepositories(basePackages = "com.frontier.repository")
 public class BackendApplication {
 
 	public static void main(String[] args) {
