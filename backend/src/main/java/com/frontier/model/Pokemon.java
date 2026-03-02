94f17617238037966b308487d1f096b03b01ba60
Alex <aaahandour@gmail.com>
2026-03-02 00:26:57 -0500

    change trainer model and sample data

--- backend/src/main/java/com/frontier/model/Pokemon.java
+++ backend/src/main/java/com/frontier/model/Pokemon.java
@@ -1,12 +1,11 @@
 package com.frontier.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
 
 import lombok.NoArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.Data;
 
 @Data
@@ -14,43 +13,17 @@ import lombok.Data;
 @Document(collection = "pokemon")
 public class Pokemon {
 
-    @Getter
-    @Setter
+    @Id
+    private String id;
+
     private String name;
-
-    @Getter
-    @Setter
     private int dex;
-
-    @Getter
-    @Setter
     private int hp;
-
-    @Getter
-    @Setter
     private int atk;
-
-    @Getter
-    @Setter
     private int def;
-
-    @Getter
-    @Setter
     private int spa;
-
-    @Getter
-    @Setter
     private int spd;
-
-    @Getter
-    @Setter
     private int spe;
-
-    @Getter
-    @Setter
     private ArrayList<String> types;
-
-    @Getter
-    @Setter
     private ArrayList<String> abilities;
 }
--- backend/src/main/java/com/frontier/model/Trainer.java
+++ backend/src/main/java/com/frontier/model/Trainer.java
@@ -1,6 +1,5 @@
 package com.frontier.model;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,7 +14,6 @@ public class Trainer {
     @Id
     private String id;
 
-    private Integer index;
     private String name;
     private String trainerClass;
     private Integer rank;
