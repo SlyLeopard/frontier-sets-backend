f2c452977b04ddb16b12edcf622d829057275f98
Alex <aaahandour@gmail.com>
2026-03-05 20:01:05 -0500

    add main search function for frontend

--- backend/src/main/java/com/frontier/controller/PokemonSetController.java
+++ backend/src/main/java/com/frontier/controller/PokemonSetController.java
@@ -2,12 +2,16 @@ package com.frontier.controller;
 
 // Imports
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.frontier.model.PokemonSet;
+import com.frontier.model.SearchCriteria;
 import com.frontier.repository.PokemonSetRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 // Annotations
 @RestController
@@ -28,6 +32,21 @@ public class PokemonSetController {
     @GetMapping("/allPokemonSets")
     public Iterable<PokemonSet> getPokemon() {
         return pokemonSetRepository.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE + "/searchPokemonSets")
+    public Iterable<PokemonSet> searchPokemonSets(@RequestBody(required = true) SearchCriteria searchCriteria) {
+        String species = searchCriteria.getSpecies();
+        Integer rank = searchCriteria.getRank();
+        if (rank != null && species != null) {
+            return pokemonSetRepository.findByRankAndSpecies(rank, species);
+        } else if (rank != null) {
+            return pokemonSetRepository.findByRank(rank);
+        } else if (species != null) {
+            return pokemonSetRepository.findBySpecies(species);
+        } else {
+            return pokemonSetRepository.findAll();
+        }
     }
 
 }
