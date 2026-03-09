a23cb0b09ccd73888a854de45c024bc28dee9d04
Alex <aaahandour@gmail.com>
2026-03-09 01:59:57 -0400

    update enum casing

--- backend/src/main/java/com/frontier/enums/Nature.java
+++ backend/src/main/java/com/frontier/enums/Nature.java
@@ -2,35 +2,35 @@ package com.frontier.enums;
 
 public enum Nature {
 
-    HARDY(null, null),
-    LONELY(Stat.ATK, Stat.DEF),
-    BRAVE(Stat.ATK, Stat.SPE),
-    ADAMANT(Stat.ATK, Stat.SPA),
-    NAUGHTY(Stat.ATK, Stat.SPD),
+    Hardy(null, null),
+    Lonely(Stat.ATK, Stat.DEF),
+    Brave(Stat.ATK, Stat.SPE),
+    Adamant(Stat.ATK, Stat.SPA),
+    Naughty(Stat.ATK, Stat.SPD),
 
-    BOLD(Stat.DEF, Stat.ATK),
-    DOCILE(null, null),
-    RELAXED(Stat.DEF, Stat.SPE),
-    IMPISH(Stat.DEF, Stat.SPA),
-    LAX(Stat.DEF, Stat.SPD),
+    Bold(Stat.DEF, Stat.ATK),
+    Docile(null, null),
+    Relaxed(Stat.DEF, Stat.SPE),
+    Impish(Stat.DEF, Stat.SPA),
+    Lax(Stat.DEF, Stat.SPD),
 
-    TIMID(Stat.SPE, Stat.ATK),
-    HASTY(Stat.SPE, Stat.DEF),
-    SERIOUS(null, null),
-    JOLLY(Stat.SPE, Stat.SPA),
-    NAIVE(Stat.SPE, Stat.SPD),
+    Timid(Stat.SPE, Stat.ATK),
+    Hasty(Stat.SPE, Stat.DEF),
+    Serious(null, null),
+    Jolly(Stat.SPE, Stat.SPA),
+    Naive(Stat.SPE, Stat.SPD),
 
-    MODEST(Stat.SPA, Stat.ATK),
-    MILD(Stat.SPA, Stat.DEF),
-    QUIET(Stat.SPA, Stat.SPE),
-    BASHFUL(null, null),
-    RASH(Stat.SPA, Stat.SPD),
+    Modest(Stat.SPA, Stat.ATK),
+    Mild(Stat.SPA, Stat.DEF),
+    Quiet(Stat.SPA, Stat.SPE),
+    Bashful(null, null),
+    Rash(Stat.SPA, Stat.SPD),
 
-    CALM(Stat.SPD, Stat.ATK),
-    GENTLE(Stat.SPD, Stat.DEF),
-    SASSY(Stat.SPD, Stat.SPE),
-    CAREFUL(Stat.SPD, Stat.SPA),
-    QUIRKY(null, null);
+    Calm(Stat.SPD, Stat.ATK),
+    Gentle(Stat.SPD, Stat.DEF),
+    Sassy(Stat.SPD, Stat.SPE),
+    Careful(Stat.SPD, Stat.SPA),
+    Quirky(null, null);
 
     private final Stat boost;
     private final Stat drop;
