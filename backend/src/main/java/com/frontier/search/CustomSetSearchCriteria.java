6a6f562cfdcd7ab7cb5185b9e547675e56bd65ad
Alex <aaahandour@gmail.com>
2026-03-09 17:38:50 -0400

    fix bug where base criteria is called twice

--- backend/src/main/java/com/frontier/search/CustomSetSearchCriteria.java
+++ backend/src/main/java/com/frontier/search/CustomSetSearchCriteria.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 public class CustomSetSearchCriteria extends BaseSetSearchCriteria {
 
     @Override
--- backend/src/main/java/com/frontier/search/PokemonSetSearchCriteria.java
+++ backend/src/main/java/com/frontier/search/PokemonSetSearchCriteria.java
@@ -1,21 +1,16 @@
 package com.frontier.search;
 
 import org.springframework.data.mongodb.core.query.Query;
-import com.frontier.enums.Nature;
 import com.frontier.search.base.BaseSetSearchCriteria;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 public class PokemonSetSearchCriteria extends BaseSetSearchCriteria{
     
-    private String species;
     private Integer rank;
-    private Nature nature;
-    private String name;
-    private String item;
     private Integer generation;
 
     @Override
@@ -25,7 +20,6 @@ public class PokemonSetSearchCriteria extends BaseSetSearchCriteria{
         addIfPresent(query, "rank", rank);
         addIfPresent(query, "generation", generation);
         addBaseSetCriteria(query);
-        addBaseCriteria(query);
         
         return query;
     }
