be79e3f2e1ef80d0a90fe78756b291b80ae811dd
Alex <aaahandour@gmail.com>
2026-03-11 19:19:11 -0400

    remove unneeded generic dto function

--- backend/src/main/java/com/frontier/service/CustomSetService.java
+++ backend/src/main/java/com/frontier/service/CustomSetService.java
@@ -1,7 +1,5 @@
 package com.frontier.service;
 
-import java.util.function.Function;
-
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +12,7 @@ import com.frontier.service.base.BaseSetService;
 @Service
 public class CustomSetService extends BaseSetService<CustomSet, CustomSetRepository, CustomSetSearchCriteria, CustomSetDTO> {
 
-    public CustomSetService(CustomSetRepository repo, MongoTemplate mongoTemplate, Function<CustomSet, CustomSetDTO> mapper) {
+    public CustomSetService(CustomSetRepository repo, MongoTemplate mongoTemplate) {
         super(repo, mongoTemplate, CustomSet.class, CustomSetDTO::fromEntity);
     }
 
