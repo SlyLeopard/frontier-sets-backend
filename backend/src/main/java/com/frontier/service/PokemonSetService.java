8ed335bf4f7c343d75a9ca46d53ef451afcee32d
Alex <aaahandour@gmail.com>
2026-03-11 18:50:39 -0400

    rework combined DTO creation to only use 2 queries

--- backend/src/main/java/com/frontier/service/PokemonSetService.java
+++ backend/src/main/java/com/frontier/service/PokemonSetService.java
@@ -1,30 +1,33 @@
 package com.frontier.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 
+import com.frontier.model.Pokemon;
 import com.frontier.model.PokemonSet;
 import com.frontier.model.dto.CombinedPokemonSetDTO;
 import com.frontier.model.dto.PokemonSetDTO;
+import com.frontier.model.dto.PokemonDTO;
 import com.frontier.repository.PokemonSetRepository;
 import com.frontier.search.PokemonSetSearchCriteria;
 import com.frontier.service.base.BaseSetService;
 
 @Service
-public class PokemonSetService extends BaseSetService<PokemonSet, PokemonSetRepository, PokemonSetSearchCriteria, PokemonSetDTO> {
-
-    private PokemonService pokemonService;
+public class PokemonSetService
+        extends BaseSetService<PokemonSet, PokemonSetRepository, PokemonSetSearchCriteria, PokemonSetDTO> {
 
     public PokemonSetService(PokemonSetRepository repo, MongoTemplate mongoTemplate, PokemonService pokemonService) {
         super(repo, mongoTemplate, PokemonSet.class, PokemonSetDTO::fromEntity);
-        this.pokemonService = pokemonService;
     }
 
     public Page<CombinedPokemonSetDTO> combinedSearch(PokemonSetSearchCriteria criteria) {
@@ -32,13 +35,28 @@ public class PokemonSetService extends BaseSetService<PokemonSet, PokemonSetRepo
         Query query = criteria.toQuery();
         Pageable pageable = criteria.toPageable();
         query.with(pageable);
-        List<PokemonSet> results = mongoTemplate.find(query, PokemonSet.class);
-        List<CombinedPokemonSetDTO> combinedResults = new ArrayList<CombinedPokemonSetDTO>();
-        for (PokemonSet set : results) {
-            combinedResults.add(CombinedPokemonSetDTO.fromEntity(set, pokemonService.getByName(set.getSpecies()).orElse(null)));
-        }
+        List<PokemonSet> sets = mongoTemplate.find(query, PokemonSet.class);
+        Set<String> pokemonNames = sets.stream().map(PokemonSet::getName).collect(Collectors.toSet());
+
+        Query pokemonNameQuery = new Query(
+                Criteria.where("name").in(pokemonNames));
+
+        List<PokemonDTO> pokemonList = mongoTemplate.find(pokemonNameQuery, Pokemon.class).stream()
+                .map(p -> PokemonDTO.fromEntity(p)).toList();
+
+        Map<String, PokemonDTO> pokemonNameMap = pokemonList.stream()
+                .collect(Collectors.toMap(PokemonDTO::getName, p -> p));
+
+        List<CombinedPokemonSetDTO> combined = sets.stream()
+                .map(set -> {
+                    PokemonDTO pokemon = pokemonNameMap.get(set.getName());
+                    return CombinedPokemonSetDTO.fromEntity(
+                            set,
+                            pokemon);
+                }).toList();
+
         return PageableExecutionUtils.getPage(
-                combinedResults,
+                combined,
                 pageable,
                 () -> mongoTemplate.count(query.skip(0).limit(0), PokemonSet.class));
     }
