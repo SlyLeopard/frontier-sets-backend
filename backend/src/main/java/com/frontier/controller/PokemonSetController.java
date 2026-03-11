6717547893aba0c2136cd48cd32e40723d95dd85
Alex <aaahandour@gmail.com>
2026-03-11 16:27:44 -0400

    finalize combinedSearch endpoint to pass species info to frontend as part of main payload

--- backend/src/main/java/com/frontier/controller/PokemonSetController.java
+++ backend/src/main/java/com/frontier/controller/PokemonSetController.java
@@ -1,21 +1,44 @@
 package com.frontier.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.frontier.controller.base.BaseController;
 import com.frontier.model.PokemonSet;
+import com.frontier.model.dto.CombinedPokemonSetDTO;
+import com.frontier.model.dto.PokemonSetDTO;
 import com.frontier.search.PokemonSetSearchCriteria;
 import com.frontier.service.PokemonSetService;
 
 @CrossOrigin(origins = "${app.frontend.url}")
 @RestController
 @RequestMapping("/pokemon-set")
-public class PokemonSetController extends BaseController<PokemonSet, PokemonSetSearchCriteria> {
+public class PokemonSetController extends BaseController<PokemonSet, PokemonSetSearchCriteria, PokemonSetDTO> {
+
+    private final PokemonSetService service;
 
     public PokemonSetController(PokemonSetService service) {
         super(service);
+        this.service = service;
+    }
+
+    @PostMapping("/v1/search")
+    public ResponseEntity<Page<CombinedPokemonSetDTO>> combinedSearch(
+            PokemonSetSearchCriteria criteria,
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer pageSize) {
+        page = page == null ? 0 : page;
+        pageSize = pageSize == null ? 20 : pageSize;
+        pageSize = pageSize > 100 ? 100 : pageSize;
+        criteria.setPage(page);
+        criteria.setSize(pageSize);
+        Page<CombinedPokemonSetDTO> results = service.combinedSearch(criteria);
+        return ResponseEntity.ok(results);
     }
 
 }
--- backend/src/main/java/com/frontier/service/PokemonSetService.java
+++ backend/src/main/java/com/frontier/service/PokemonSetService.java
@@ -1,18 +1,78 @@
 package com.frontier.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 
+import com.frontier.model.Pokemon;
 import com.frontier.model.PokemonSet;
+import com.frontier.model.dto.CombinedPokemonSetDTO;
+import com.frontier.model.dto.PokemonSetDTO;
 import com.frontier.repository.PokemonSetRepository;
 import com.frontier.search.PokemonSetSearchCriteria;
 import com.frontier.service.base.BaseSetService;
 
 @Service
-public class PokemonSetService extends BaseSetService<PokemonSet, PokemonSetRepository, PokemonSetSearchCriteria> {
+public class PokemonSetService extends BaseSetService<PokemonSet, PokemonSetRepository, PokemonSetSearchCriteria, PokemonSetDTO> {
 
-    public PokemonSetService(PokemonSetRepository repo, MongoTemplate mongoTemplate) {
+    private PokemonService pokemonService;
+
+    public PokemonSetService(PokemonSetRepository repo, MongoTemplate mongoTemplate, PokemonService pokemonService) {
         super(repo, mongoTemplate, PokemonSet.class);
+        this.pokemonService = pokemonService;
     }
+
+    public Page<CombinedPokemonSetDTO> combinedSearch(PokemonSetSearchCriteria criteria) {
+
+        Query query = criteria.toQuery();
+        Pageable pageable = criteria.toPageable();
+        query.with(pageable);
+        List<PokemonSet> results = mongoTemplate.find(query, PokemonSet.class);
+        List<CombinedPokemonSetDTO> combinedResults = new ArrayList<CombinedPokemonSetDTO>();
+        for (PokemonSet set : results) {
+            combinedResults.add(toCombinedDTO(set));
+        }
+        return PageableExecutionUtils.getPage(
+                combinedResults,
+                pageable,
+                () -> mongoTemplate.count(query.skip(0).limit(0), PokemonSet.class));
+    }
+
+    public PokemonSetDTO toDTO(PokemonSet set) {
+        PokemonSetDTO dto = new PokemonSetDTO();
+        dto.setName(set.getName());
+        dto.setSpecies(set.getSpecies());
+        dto.setNature(set.getNature());
+        dto.setItem(set.getItem());
+        dto.setEv(set.getEv());
+        dto.setMoves(set.getMoves());
+        dto.setRank(set.getRank());
+        dto.setGeneration(set.getGeneration());
+        return dto;
+    }
+
+    public CombinedPokemonSetDTO toCombinedDTO(PokemonSet set) {
+        CombinedPokemonSetDTO dto = new CombinedPokemonSetDTO();
+        Optional<Pokemon> optional = pokemonService.getByName(set.getSpecies());
+        dto.setName(set.getName());
+        dto.setSpecies(set.getSpecies());
+        dto.setPokemon(pokemonService.toDTO(optional.orElse(null)));
+        dto.setNature(set.getNature());
+        dto.setItem(set.getItem());
+        dto.setEv(set.getEv());
+        dto.setMoves(set.getMoves());
+        dto.setRank(set.getRank());
+        dto.setGeneration(set.getGeneration());
+        return dto;
+    }
+
+
 
 }
